8c1e223568e88c6b1df923864a5de1444a13c8de
Tara <75340062+traopand@users.noreply.github.com>
2021-02-28 16:57:28 -0500

    dashboard changes

--- Screens/Dashboard.js
+++ Screens/Dashboard.js
@@ -1,12 +1,45 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, Alert } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Alert,
+  Modal,
+  Pressable,
+  Switch,
+  Image,
+  Button,
+} from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import * as firebase from "firebase";
 import { loggingOut } from "../API/firebaseMethods";
+import { GiftedChat } from "react-native-gifted-chat";
+import { Input } from "react-native-elements";
+import Logo from "../assets/Logo.png";
+import BillIcon from "../assets/BillIcon.png";
+import GroceryIcon from "../assets/groceryIcon.png";
+import AddMember from "../assets/AddMember.png";
+import Member1 from "../assets/Member1.png";
+import Member2 from "../assets/Member2.png";
+import Member3 from "../assets/Member3.png";
+import Member4 from "../assets/Member4.png";
+import Member5 from "../assets/Member5.png";
+import Member6 from "../assets/Member6.png";
+import Rectangle from "../assets/Rectangle.png";
 
 export default function Dashboard({ navigation }) {
   let currentUserUID = firebase.auth().currentUser.uid;
   const [firstName, setFirstName] = useState("");
+  const [createGroupVisible, setCreateGroupVisible] = useState(false);
+  const [groupName, setGroupName] = useState("Pod Name");
+  const [listMembers, setListMembers] = useState("");
+
+  // TUTORIAL GROUP CREATOR //
+  const [group, setGroup] = useState("");
+
+  const [listName, setListName] = useState("List Name");
+  const [infoVisible, setInfoVisible] = useState(false);
+  const listRef = firebase.firestore().collection("groceryList");
 
   useEffect(() => {
     async function getUserInfo() {
@@ -35,13 +68,106 @@ export default function Dashboard({ navigation }) {
     navigation.replace("GroceryList");
   };
 
+  const handlePress = () => {
+    loggingOut();
+    navigation.replace("Login");
+  };
+
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
+  async function addList() {
+    await listRef.add({
+      title: listName,
+      admin: firstName,
+      members: listMembers,
+      evenSplit: isEnabled,
+    });
+    setListName("");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Dashboard</Text>
-      <Text>Hi {firstName}</Text>
+      <Image style={styles.logo} source={Logo} />
+      <Text style={styles.titleText}>Ravenous Raccoons</Text>
+      <Text style={styles.nameText}>Hi {firstName}</Text>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={createGroupVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!createGroupVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Input
+              inputContainerStyle={{
+                borderBottomColor: "#A4BEAD",
+                borderBottomWidth: 1.5,
+              }}
+              onChangeText={(listName) => setListName(listName)}
+              value={listName}
+              style={styles.listName}
+            ></Input>
 
-      <TouchableOpacity style={styles.button} onPress={gotoGroceryList}>
-        <Text style={styles.logout}>Go to List</Text>
+            <View style={{ flexDirection: "row" }}>
+              <Text style={styles.evenSplit}>Even Split Mode </Text>
+              <Switch
+                trackColor={{ false: "#767577", true: "#B4B7FF" }}
+                thumbColor={isEnabled ? "#E1EFD6" : "#f4f3f4"}
+                ios_backgroundColor="#3e3e3e"
+                onValueChange={toggleSwitch}
+                value={isEnabled}
+              />
+            </View>
+
+            <TouchableOpacity onPress={addList} style={[styles.button]}>
+              <Text style={styles.textStyle}>Add List</Text>
+            </TouchableOpacity>
+            <Pressable
+              style={[styles.button]}
+              onPress={() => setCreateGroupVisible(!createGroupVisible)}
+            >
+              <Text style={styles.textStyle}>Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
+      <View style={styles.quickAdd}>
+        <Image style={styles.rectangle} source={Rectangle} />
+        <Text style={styles.quickAddText}>Quick Add</Text>
+        <Input
+          inputContainerStyle={{
+            borderBottomColor: "#C5C5C7",
+            paddingHorizontal: 10,
+            width: "70%",
+            marginLeft: 40,
+            marginRight: 40,
+          }}
+          style={styles.textInput}
+        >
+          Enter Item
+        </Input>
+        <TouchableOpacity style={styles.addButton}>
+          <Text style={styles.logout}>Add</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setCreateGroupVisible(true)}
+      >
+        <Text style={styles.logout}>Create List</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("GroceryList")}
+      >
+        <Text style={styles.logout}>Go to list</Text>
       </TouchableOpacity>
 
       <TouchableOpacity style={styles.button} onPress={logout}>
@@ -59,24 +185,145 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 
+  logo: {
+    width: 144,
+    height: 172,
+  },
+
+  titleText: {
+    fontSize: 35,
+    fontWeight: "bold",
+    color: "#5C7F7B",
+    marginTop: -30,
+    paddingBottom: 10,
+  },
+
+  rectangle: {
+    width: "55%",
+    height: "16%",
+    marginLeft: "22%",
+    marginRight: "22%",
+    marginTop: -80,
+    marginBottom: 15,
+  },
+
+  nameText: {
+    fontSize: 20,
+    color: "#5C7F7B",
+    paddingBottom: 25,
+  },
+
+  quickAdd: {
+    backgroundColor: "#E1EFD6",
+    width: "70%",
+    height: "30%",
+    paddingTop: 20,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+
+  quickAddText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+
+  textInput: {
+    paddingTop: 10,
+    fontSize: 15,
+    paddingBottom: 10,
+    textAlign: "center",
+    color: "#393C35",
+  },
+
   titleContainer: {
     marginBottom: 40,
     alignItems: "center",
     justifyContent: "center",
   },
 
-  button: {
+  addButton: {
     backgroundColor: "#B4B7FF",
-    marginTop: 70,
-    width: 343,
+    marginTop: 30,
+    width: 150,
     height: 56,
-    borderRadius: 14,
+    borderRadius: 30,
+    marginLeft: 70,
+    marginRight: 70,
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  button: {
+    backgroundColor: "#B4B7FF",
+    marginTop: 30,
+    width: 150,
+    height: 56,
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    borderColor: "#A4BEAD",
+    borderWidth: 2,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    width: "90%",
+    height: "40%",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  listName: {
+    marginBottom: 10,
+    textAlign: "center",
+    fontSize: 36,
+    color: "#5D7E7D",
+    fontWeight: "bold",
+  },
+
+  evenSplit: {
+    fontSize: 25,
+    paddingRight: 10,
+    color: "#5D7E7D",
   },
 
   logout: {
     fontSize: 18,
     color: "white",
+  },
+
+  evenSplitIcon: {
+    width: 30,
+    height: 30,
+    marginLeft: 20,
   },
 });
--- Screens/GroceryList.js
+++ Screens/GroceryList.js
@@ -1,177 +1,208 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, FlatList, View, Text , Alert,  Image } from 'react-native';
-import GroceryItem from './GroceryItem';
-
+import React, { useEffect, useState } from "react";
+import { StyleSheet, FlatList, View, Text, Alert, Image } from "react-native";
+import GroceryItem from "./GroceryItem";
+import { Input } from "react-native-elements";
 import * as firebase from "firebase";
-import { Appbar, Button, TextInput } from 'react-native-paper';
+import { Appbar, Button, TextInput } from "react-native-paper";
 import Logo from "../assets/Logo.png";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function GroceryList({ navigation }) {
-    let currentUserUID = firebase.auth().currentUser.uid;
-    const [firstName, setFirstName] = useState("Amanda");
+  let currentUserUID = firebase.auth().currentUser.uid;
+  const [firstName, setFirstName] = useState("Amanda");
 
-    const [groceryItem, setGroceryItem] = useState('');
-    // const groupRef = firebase.firestore().collection("group/Rodaxem1mzuhpqAOq25u");
-    const ref = firebase.firestore().collection("groceryList/UMa1GQigE73aEWGC9dUM/itemCollection");
+  const [groceryItem, setGroceryItem] = useState("");
+  // const groupRef = firebase.firestore().collection("group/Rodaxem1mzuhpqAOq25u");
+  const ref = firebase
+    .firestore()
+    .collection("groceryList/UMa1GQigE73aEWGC9dUM/itemCollection");
 
-    const [groceryItemName, setGroceryItemName] = useState("");
+  const [groceryItemName, setGroceryItemName] = useState("");
 
-    const [loading, setLoading] = useState(true);
-    const [groceryList, setGroceryList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [groceryList, setGroceryList] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
+    async function getUserInfo() {
+      let doc = await firebase
+        .firestore()
+        .collection("usersList")
+        .doc(currentUserUID)
+        .get();
 
-        // async function getUserInfo() {
-        //     let doc = await firebase
-        //       .firestore()
-        //       .collection("usersList")
-        //       .doc(currentUserUID)
-        //       .get();
-      
-        //     if (!doc.exists) {
-        //       Alert.alert("No user data found!");
-        //     } else {
-        //       let dataObj = doc.data();
-        //       setFirstName(dataObj.firstName);
-        //     }
-        //   }
-        //   getUserInfo();
-          
-        async function getGroupInfo() {
-            let doc = await firebase
-              .firestore()
-              .collection("groceryList")
-              .doc("UMa1GQigE73aEWGC9dUM")
-              .get();
-      
-              // THE GROCERY LIST ID IS CURRENTLY HARDED CODED^^
-            if (!doc.exists) {
-              Alert.alert("No grocery list name found!");
-            } else {
-              let dataObj = doc.data();
-              setGroceryItemName(dataObj.groceryListName);
-            }
-          }
-          getGroupInfo();
-
-        return ref.onSnapshot(querySnapshot => {
-            const list = [];
-            querySnapshot.forEach(doc => {
-                const { itemName, quantity, addedBy} = doc.data();
-                list.push({
-                    id: doc.id,
-                    itemName,
-                    quantity,
-                    addedBy,
-                });
-            });
-
-            setGroceryList(list);
-
-            if (loading) {
-                setLoading(false);
-            }
-        });
-    }, []);
-
-    async function addGroceryItem() {
-        await ref.add({
-            itemName: groceryItem,
-            quantity: 0,
-            addedBy: firstName,
-        });
-        setGroceryItem('');
+      if (!doc.exists) {
+        Alert.alert("No user data found!");
+      } else {
+        let dataObj = doc.data();
+        setFirstName(dataObj.firstName);
+      }
     }
+    getUserInfo();
 
-    const handleDashboard = () => {
-        navigation.replace("Dashboard");
-      };
+    async function getGroupInfo() {
+      let doc = await firebase
+        .firestore()
+        .collection("groceryList")
+        .doc("UMa1GQigE73aEWGC9dUM")
+        .get();
 
-    if (loading) {
-        return null; // or a spinner
+      // THE GROCERY LIST ID IS CURRENTLY HARDED CODED^^
+      if (!doc.exists) {
+        Alert.alert("No grocery list name found!");
+      } else {
+        let dataObj = doc.data();
+        setGroceryItemName(dataObj.groceryListName);
+      }
     }
-    return (
-        <View style={styles.container}>
+    getGroupInfo();
 
-            <Button onPress={() => handleDashboard()}>
-                <Text>Back</Text>
-            </Button>
+    return ref.onSnapshot((querySnapshot) => {
+      const list = [];
+      querySnapshot.forEach((doc) => {
+        const { itemName, quantity, addedBy } = doc.data();
+        list.push({
+          id: doc.id,
+          itemName,
+          quantity,
+          addedBy,
+        });
+      });
 
-            <Image style={styles.logo} source={Logo} />
+      setGroceryList(list);
 
-            <Text style={styles.h1}>{groceryItemName}</Text>
-            <Text style={styles.shopper}>Shopper: Kathy Cao</Text>
-           
-           {/* name is currently hardcoded!*/}
+      if (loading) {
+        setLoading(false);
+      }
+    });
+  }, []);
 
-            {/* Table headings*/}
-            <View style={{ flexDirection: 'row',  justifyContent: 'center'}}>
-                <Text style={styles.h2}>Item</Text>
-                <Text style={styles.h2}>Quantity</Text>
-                <Text style={styles.h2}>Added By</Text>
-            </View>
-            
-            {/* name is currently hardcoded!*/}
-            <FlatList
-                style={{ flex: 1 }}
-                data={groceryList}
-                keyExtractor={(item) => item.id}
-                renderItem={({ item }) => <GroceryItem {...item} />}
-            />
-            <TextInput label={'New Item'} value={groceryItem} onChangeText={setGroceryItem} />
-           
-            <View style={{ flexDirection: 'row', marginHorizontal: -5}}>
-                <Button onPress={() => addGroceryItem()} style={styles.button}>
-                        <Text style={styles.p}>Add Item +</Text>
-                </Button>
-                <Button onPress={() => addGroceryItem()} style={styles.button}>
-                        <Text style={styles.p}>Claim Items</Text>
-                </Button>
-            </View>
-            
-        </View>
-    );
+  async function addGroceryItem() {
+    await ref.add({
+      itemName: groceryItem,
+      quantity: 0,
+      addedBy: firstName,
+    });
+    setGroceryItem("");
+  }
+
+  const handleDashboard = () => {
+    navigation.replace("Dashboard");
+  };
+
+  if (loading) {
+    return null; // or a spinner
+  }
+
+  //UPDATE LIST NAME:
+  async function updateName(groceryItemName) {
+    await firebase
+      .firestore()
+      .collection("groceryList")
+      .doc("UMa1GQigE73aEWGC9dUM")
+      .update({
+        groceryListName: groceryItemName,
+      });
+    // THE GROCERY LIST ID IS CURRENTLY HARDED CODED^^
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button onPress={() => handleDashboard()}>
+        <Text>Back</Text>
+      </Button>
+
+      <Image style={styles.logo} source={Logo} />
+
+      <Input
+        inputContainerStyle={{
+          borderBottomColor: "transparent",
+        }}
+        onChangeText={updateName}
+      >
+        <Text style={styles.h1}>{groceryItemName}</Text>
+      </Input>
+      <TouchableOpacity
+        onPress={updateName}
+        value={groceryItemName}
+      ></TouchableOpacity>
+
+      <Text style={styles.shopper}>Shopper: Kathy Cao</Text>
+
+      {/* name is currently hardcoded!*/}
+
+      {/* Table headings*/}
+      <View style={{ flexDirection: "row", justifyContent: "center" }}>
+        <Text style={styles.h2}>Item</Text>
+        <Text style={styles.h2}>Quantity</Text>
+        <Text style={styles.h2}>Added By</Text>
+      </View>
+
+      {/* name is currently hardcoded!*/}
+      <FlatList
+        style={{ flex: 1 }}
+        data={groceryList}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GroceryItem {...item} />}
+      />
+      <TextInput
+        label={"New Item"}
+        value={groceryItem}
+        onChangeText={setGroceryItem}
+      />
+
+      <View style={{ flexDirection: "row", marginHorizontal: -5 }}>
+        <Button onPress={() => addGroceryItem()} style={styles.button}>
+          <Text style={styles.p}>Add Item +</Text>
+        </Button>
+        <Button onPress={() => addGroceryItem()} style={styles.button}>
+          <Text style={styles.p}>Claim Items</Text>
+        </Button>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        
-        backgroundColor: '#fff',
-        // alignItems: 'stretch',
-        justifyContent: 'center',
-        padding: 30,
-    },
-    h1: {
-        marginBottom: '5%',
-        fontSize: 20,
-        color: '#5C7F7B',
-        fontWeight: 'bold',
-    },
-    h2: {
-        flex: 1,
-        marginTop: '10%',
-        fontSize: 15,
-        color: '#5C7F7B',
-        fontWeight: 'bold',
-    },
-    p:{
-        color: '#fff',
-    },
-    shopper: {
-        alignSelf: 'flex-end',
-        color: '#5D7E7D',
-    },
-    button: {
-        flex: 1,
-        marginTop: '10%',
-        marginHorizontal: 5,
-        backgroundColor: '#B4B7FF',   
-    },
-    logo: {
-        alignSelf: 'center',
-        width: 170,
-        height: 215,
-        margin: 0,
-    },
-})
+  container: {
+    flex: 1,
+
+    backgroundColor: "#fff",
+    // alignItems: 'stretch',
+    justifyContent: "center",
+    padding: 30,
+  },
+  h1: {
+    marginVertical: "5%",
+    fontSize: 25,
+    color: "#5C7F7B",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  h2: {
+    flex: 1,
+    marginTop: "10%",
+    fontSize: 15,
+    color: "#5C7F7B",
+    fontWeight: "bold",
+  },
+  p: {
+    color: "#fff",
+  },
+  shopper: {
+    alignSelf: "flex-end",
+    color: "#5D7E7D",
+  },
+  button: {
+    flex: 1,
+    marginTop: "10%",
+    marginHorizontal: 5,
+    backgroundColor: "#B4B7FF",
+  },
+  logo: {
+    alignSelf: "center",
+    // borderColor:'brown',
+    // borderWidth: 1,
+    width: 170,
+    height: 215,
+  },
+});
